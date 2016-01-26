06bd5e3184d58f56d24268f11fd66f3192b72c3b
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-01-27 00:10:22 +0900

    ファイルダウンロード用

--- downloadFile.py
+++ downloadFile.py
@@ -4,4 +4,7 @@ import urllib.request
 def download(url, title):
     urllib.request.urlretrieve(url, title)
 
-download("https://retrip.s3.amazonaws.com/article/5324/images/5324b7fa81a5-4a73-40f1-812f-742f7ed7e429_m.jpg", "ゴリラ.jpg")
+file = open("data/input.txt", "r", encoding="UTF-8")
+for line in file:
+    line.split()
+    download(line.split()[0], "data/" + line.split()[1])
