05695ec745bb6b20cea874b466adabc495d50b6f
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-01-28 23:15:56 +0900

    csvを読み込むようにした

--- downloadFile.py
+++ downloadFile.py
@@ -1,10 +1,12 @@
 __author__ = 'kawakami_note'
 import urllib.request
+import csv
 
 def download(url, title):
     urllib.request.urlretrieve(url, title)
 
-file = open("data/input.txt", "r", encoding="UTF-8")
-for line in file:
-    line.split()
-    download(line.split()[0], "data/" + line.split()[1])
+file = open("data/input.csv", "r", encoding="UTF-8")
+lines = csv.reader(file, delimiter=",")
+
+for line in lines:
+    download(line[0], "data/" + line[1])
