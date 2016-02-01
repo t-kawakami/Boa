95c620daf5097bfa2e2a9b44b732a93cf47d86c3
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-02-01 23:29:10 +0900

    ファイル名を連番にして、マッピングファイルを出力する

--- downloadFile.py
+++ downloadFile.py
@@ -5,8 +5,21 @@ import csv
 def download(url, title):
     urllib.urlretrieve(url, title)
 
-file = open("data/input.csv", "r")
-lines = csv.reader(file, delimiter=",")
+def create_mapping_file(file_name, tags):
+    mapping_file = open("data/mapping.csv", "w")
+    writer = csv.writer(mapping_file)
+    writer.writerow([file_name, tags])
 
-for line in lines:
-    download(line[0], "data/" + line[1])
+def main():
+    input_file = open("data/input.csv", "r")
+    lines = csv.reader(input_file, delimiter=",")
+
+    index = 0
+    for line in lines:
+        index += 1
+        file_name = "data/images/" + str(index).zfill(10) + ".jpg"
+        download(line[0], file_name)
+        create_mapping_file(file_name, line[1])
+
+if __name__ == "__main__":
+    main()
