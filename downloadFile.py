3437e021948078b3a06c76db3e2daf9662d4c34d
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-02-05 01:33:22 +0900

    書き込み修正

--- downloadFile.py
+++ downloadFile.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'kawakami_note'
 import urllib
 import csv
@@ -5,21 +6,22 @@ import csv
 def download(url, title):
     urllib.urlretrieve(url, title)
 
-def create_mapping_file(file_name, tags):
-    mapping_file = open("data/mapping.csv", "w")
-    writer = csv.writer(mapping_file)
-    writer.writerow([file_name, tags])
-
-def main():
+def main(from_index):
     input_file = open("data/input.csv", "r")
+    mapping_file = open("data/mapping.csv", "w")
     lines = csv.reader(input_file, delimiter=",")
 
     index = 0
     for line in lines:
         index += 1
+        if index <= from_index:
+            continue
         file_name = "data/images/" + str(index).zfill(10) + ".jpg"
+        print('processing:' + file_name)
         download(line[0], file_name)
-        create_mapping_file(file_name, line[1])
+        # TODO 余計に改行が入ってしまう。
+        writer = csv.writer(mapping_file, delimiter=',',quotechar='|')
+        writer.writerow([file_name, line[1]])
 
 if __name__ == "__main__":
-    main()
+    main(0)
