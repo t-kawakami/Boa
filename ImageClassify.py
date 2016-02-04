53a61a79e5b77092ba0f9403c24f6aa2f9b13aa0
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-02-05 00:27:25 +0900

    各要素をクラスタのもっとも近いもので分類

--- ImageClassify.py
+++ ImageClassify.py
@@ -5,22 +5,40 @@ import numpy as np
 import csv
 import cv2
 
-def create_hist(key_points_all):
+def create_hist(all_key_points, centers):
+    hists = {}
+    for key in all_key_points:
+        hist = [0] * len(centers)
+        for key_point in all_key_points[key]:
+            min_dimension = -1
+            min_index = 0
+            index = 0
+            for center in centers:
+                dimension = np.sqrt(np.add(np.power(np.subtract(key_point[0], center[0]), 2),np.power(np.subtract(key_point[1], center[1]), 2)))
+                if min_dimension == -1 or min_dimension > dimension:
+                    min_dimension = dimension
+                    min_index = index
+                index += 1
+            hist[min_index] += 1
+        hists[key] = hist
+    return hists
+
+def create_cluster(key_points_all):
     # 特徴点をKMean法でクラスタ化
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
     ret, label, center = cv2.kmeans(key_points_all, 15, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-
-    a = key_points_all[label.ravel() == 0]
-    b = key_points_all[label.ravel() == 1]
-
+    return center
 
 def extract_key_points(img_path):
     img = cv2.imread(img_path)
     detector = cv2.FastFeatureDetector_create()
     img_detects = detector.detect(img)
-    return np.array(img_detect.pt for img_detect in img_detects)
+    key_points = []
+    for img_detect in img_detects:
+        key_points.append(img_detect.pt)
+    return np.array(key_points)
 
-if __name__ == '__main__':
+def main():
     # 画像のデータファイル読み込み
     mapping_file = open('data/mapping.csv', 'r')
     reader = csv.reader(mapping_file)
@@ -28,9 +46,15 @@ if __name__ == '__main__':
     # 特徴点とタグを取得
     key_points = {}
     img_tags = {}
+    key_points_all = np.array(np.zeros((0, 2)))
     for line in reader:
         key_points[line[0]] = extract_key_points(line[0])
         img_tags[line[0]] = line[1]
-        key_points['all'] = np.vstack(np.append(key_points[line[0]]))
+        key_points_all = np.vstack((key_points_all, key_points[line[0]]))
+    # 各特徴点をクラスタ化し、各クラスタの中心点を得る
+    centers = create_cluster(np.float32(key_points_all))
+    hists = create_hist(key_points, centers)
+    print(hists)
 
-    create_hist(key_points['all'])
+if __name__ == '__main__':
+    main()
--- loopPractice.py
+++ loopPractice.py
@@ -1,5 +1,7 @@
 __author__ = 'kawakami_note'
 
+import numpy as np
+
 words = ['cat', 'window', 'defenestrate']
 for word in words:
     print(word, len(word))
@@ -27,4 +29,22 @@ def loopTest():
                 break
             print(index, count)
 
-loopTest()
+def np_practice():
+    randoms = np.random.randint(25, 50, (10, 2))
+    centers = np.random.randint(25, 50, (10, 2))
+    print(randoms)
+    print(centers)
+    for random in randoms:
+        min_dimension = -1
+        index = 0
+        min_index = 0
+        for center in centers:
+            dimension = np.sqrt(np.add(np.power(random[0] - center[0], 2), np.power(random[1] - center[1], 2)))
+            if min_dimension == -1 or min_dimension > dimension:
+                min_dimension = dimension
+                min_index = index
+            index += 1
+        print(str(min_index), str(min_dimension))
+
+
+np_practice()
