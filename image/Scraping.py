af5d47273678bc958a76c0b10ed4efc0a628632c
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-02-19 22:51:05 +0900

    ファイルに出力し、サイトへのアクセスを減らせるようにした

--- image/Scraping.py
+++ image/Scraping.py
@@ -5,18 +5,30 @@ import pandas as pd
 import urllib2
 from bs4 import BeautifulSoup
 
-def extract_img_src():
+# サイトのHTMLを取得し、ファイルに出力する。
+def get_html():
     csv = pd.read_csv('data/scraping.csv')
     file_names, urls= csv['file'], csv['url']
     for file_name, url in zip(file_names, urls):
-        html = urllib2.urlopen(url).read()
+        write_file = open(file_name, 'w')
+        write_file.write(urllib2.urlopen(url).read())
+
+# img srcを抽出する
+def extract_img_src():
+    csv = pd.read_csv('data/scraping.csv')
+    file_names, tags = csv['file'], csv['tag']
+    for file_name, tag in zip(file_names, tags):
+        html_file = open(file_name, 'r')
+        html = ''.join(html_file.readlines())
         soup = BeautifulSoup(html, 'lxml')
         img_all = soup.find_all('img')
         for img in img_all:
             print(img['src'])
 
-def main():
+def main(read_html=True):
+    if read_html:
+        get_html()
     extract_img_src()
 
 if __name__ == '__main__':
-    main()
+    main(read_html=False)
