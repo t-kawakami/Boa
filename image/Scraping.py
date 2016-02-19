a7f2ea272b13f224ffaba0a742c92ceae07d2c65
kawakami <t.kawakami-010308@softbank.ne.jp>
2016-02-19 22:48:57 +0900

    アルゴリズムをlxmlに修正

--- image/Scraping.py
+++ image/Scraping.py
@@ -5,31 +5,18 @@ import pandas as pd
 import urllib2
 from bs4 import BeautifulSoup
 
-# サイトのHTMLを取得し、ファイルに出力する。(JavaScriptで展開するHTMLが含まれるため)
-def get_html():
+def extract_img_src():
     csv = pd.read_csv('data/scraping.csv')
     file_names, urls= csv['file'], csv['url']
     for file_name, url in zip(file_names, urls):
-        write_file = open(file_name, 'w')
-        write_file.write(urllib2.urlopen(url).read())
-
-
-# img srcを抽出する
-def extract_img_src():
-    csv = pd.read_csv('data/scraping.csv')
-    file_names, tags = csv['file'], csv['tag']
-    for file_name, tag in zip(file_names, tags):
-        html_file = open(file_name, 'r')
-        html = ''.join(html_file.readlines())
-        soup = BeautifulSoup(html, 'html.parser')
+        html = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(html, 'lxml')
         img_all = soup.find_all('img')
         for img in img_all:
-            print(img)
+            print(img['src'])
 
-def main(read_html=True):
-    if read_html:
-        get_html()
+def main():
     extract_img_src()
 
 if __name__ == '__main__':
-    main(read_html=True)
+    main()
